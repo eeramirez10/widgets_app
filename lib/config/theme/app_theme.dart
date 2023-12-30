071f0f4c2caff9aad3bceb6c8b15d833992103eb
Erick <eeramirez@tuvansa.com.mx>
2023-12-29 22:20:37 -0600

    Modificamos estilos de Appbar

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter/material.dart';
+
+const colorsList = <Color> [
+  Colors.green,
+  Colors.teal,
+  Colors.blue,
+  Colors.orange,
+  Colors.deepPurple
+];
+
+class AppTheme {
+  final int selectedColor;
+  AppTheme({
+    this.selectedColor = 0
+  }): assert ( selectedColor >= 0, 'Color must be greather than 0'),
+  assert (selectedColor < colorsList.length, 'Color must be less or equal than ${colorsList.length - 1}');
+
+
+  ThemeData getTheme () {
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: colorsList[selectedColor],
+      appBarTheme: AppBarTheme(
+        centerTitle: false
+      )
+    );
+  }
+
+
+}
