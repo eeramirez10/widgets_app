d0ecc84a3abdc4c759f7f8410228ad92f4e98efd
Erick <eeramirez@tuvansa.com.mx>
2024-01-14 13:17:26 -0600

    Screens creadas

---
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTutorialScreeen extends StatelessWidget {
+  static const name = 'tutorial_screen';
+  const AppTutorialScreeen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class UiControlsScreen extends StatelessWidget {
+  static const name = 'uiControls_screen';
+  const UiControlsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
